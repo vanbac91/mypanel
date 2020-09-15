e692ca36f475ad11744863c6c4b6be64bf1a3165
vanbac91 <vanbac91@gmail.com>
2020-09-15 20:22:26 +0700

    Update crack_bt_panel_pro.sh

--- crack_bt_panel_pro.sh
+++ crack_bt_panel_pro.sh
@@ -20,26 +20,26 @@ echo "Neu co vi pham lien he tac gia"
 echo "Sau khi cat dat dung thu"
 echo ""
 echo -e "${yellow}[MoTa]"
-echo -e "${plain}本脚本必须在完全干净的 CentOS/Debian/Ubuntu 系统上安装"
-echo "如已安装更高版本的宝塔面板，请先卸载高版本再安装"
-echo "如已安装其他种类的面板，或 LNMP 之类的运行环境、一键包，建议备份好数据，重装干净系统再安装"
-echo "使用本脚本出现的任何不良后果，本人概不负责"
+echo -e "${plain}This script must be completely clean Install on the system CentOS/Debian/Ubuntu "
+echo "If a higher version has been installed, please uninstall the higher version before installing"
+echo "If you have installed other types of panels, or operating environment such as LNMP, reinstall the clean system and then install"
+echo "I am not responsible for any adverse consequences of using this script"
 echo ""
-echo -e "${blue}[支持]"
+echo -e "${blue}[StandBy]"
 echo -e "${plain}zhihu: https://www.zhihu.com/people/deepdarkfantastic"
 echo "email: net.core@outlook.com"
 echo ""
 
-#确认安装
+#Confirm installation
 while [ "$go" != 'y' ] && [ "$go" != 'n' ]
 do
-    read -p "确定要安装吗？(y/n): " go;
+    read -p "Are you sure you want to install?(y/n): " go;
 done
 if [ "$go" = 'n' ];then
     exit;
 fi
 
-#检查系统信息
+#Check system information
 if [ -f /etc/redhat-release ] && [[ `grep -i 'centos' /etc/redhat-release` ]]; then
     OS='CentOS'
 elif [ ! -z "`cat /etc/issue | grep bian`" ];then
@@ -47,42 +47,42 @@ elif [ ! -z "`cat /etc/issue | grep bian`" ];then
 elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
     OS='Ubuntu'
 else
-    echo -e "${red}[错误]${plain} 你的操作系统不受支持，请选择在 Ubuntu/Debian/CentOS 操作系统上安装！"
+    echo -e "${red}[error]${plain} Your operating system is not supported, please select Ubuntu/Debian/CentOS Install on the operating system!"
     exit 1
 fi
 
-#禁用SELinux
+#Disable SELinux
 if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
     setenforce 0
 fi
 
-#输出 centos 系统大版本号
+#Output the major version number of the centos system
 System_CentOS=`rpm -q centos-release|cut -d- -f1`
 CentOS_Version=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
 
-#CentOS 6 专用 python
+#CentOS 6 dedicated python
 install_python_for_CentOS6() {
     py_for_centos="https://raw.githubusercontent.com/leitbogioro/SSR.Go/master/python_for_centos6.sh"
     py_intall="python_for_centos6.sh"
     yum install wget -y
     wget ${py_for_centos}
     if ! wget ${py_for_centos}; then
-        echo -e "[${red}错误${plain}] ${py_file} 下载失败，请检测你的网络！"
+        echo -e "[${red}error${plain}] ${py_file} The download failed, please check your network!"
         exit 1
     fi
     bash ${py_intall}
     rm -rf /root/${py_intall}
 }
 
-#CentOS 7 专用 pip 源
+#CentOS 7 Dedicated pip source
 install_python_for_CentOS7() {
     pip_file="get-pip.py"
     pip_url="https://bootstrap.pypa.io/get-pip.py"
     yum install python -y
     curl ${pip_url} -o ${pip_file}
     if ! curl ${pip_url} -o ${pip_file}; then
-        echo -e "[${red}错误${plain}] ${pip_file} 下载失败，请检测你的网络！"
+        echo -e "[${red}error${plain}] ${pip_file} The download failed, please check your network!"
         exit 1
     fi
     python ${pip_file}
@@ -99,17 +99,17 @@ install_btPanel_for_APT() {
     wget -O update.sh https://raw.githubusercontent.com/vanbac91/Crack_BT_Panel2020/master/update_pro/update_pro.sh && bash update.sh pro
 }
 
-#破解步骤
+#Cracking steps
 crack_bt_panel() {
     export Crack_file=/www/server/panel/class/common.py
-    echo -e "${yellow}[注意] ${plain}破解执行中..."
+    echo -e "${yellow}[note] ${plain}Cracking execution..."
     /etc/init.d/bt stop
     sed -i $'164s/panelAuth.panelAuth().get_order_status(None)/{\'status\': \True, \'msg\': {\'endtime\': 32503651199}}/g' ${Crack_file}
     touch /www/server/panel/data/userInfo.json
     /etc/init.d/bt restart
 }
 
-#定时重启宝塔面板
+#Restart the pagoda panel regularly
 execute_bt_panel() {
     if ! grep '/etc/init.d/bt restart' /etc/crontab; then
         systemctl enable cron.service
@@ -119,7 +119,7 @@ execute_bt_panel() {
     fi
 }
 
-#开启 ssl
+#Turn on ssl
 enable_ssl(){
     if [ ! -f /www/server/panel/data/ssl.pl ]; then
         echo "Ture" > /www/server/panel/data/ssl.pl
@@ -128,18 +128,18 @@ enable_ssl(){
     fi
 }
 
-# 安装后清理
+# Clean up after installation
 clean_up() {
     rm -rf crack_bt_panel_pro.sh
     rm -rf update.sh
     if [[ ${OS} == 'Ubuntu' ]] || [[ ${OS} == 'Debian' ]]; then
         apt-get autoremove -y
     fi
-    # 删除各类残留
+    # Delete all kinds of residue
     rm -rf /www/server/panel/plugin/btyw /root/install_cjson.sh /root/.pip /root/.pydistutils.cfg
 }
 
-# 预安装组件
+# Pre-installed components
 components(){
     cd /root
     wget -O lib.sh https://raw.githubusercontent.com/vanbac91/Crack_BT_Panel2020/master/lib.sh
@@ -153,9 +153,9 @@ components(){
     fi
 }
 
-# 插件配置
+# Plugin configuration
 vip_plugin(){
-    # 默认安装所有付费高级插件
+    # Install all paid premium plugins by default
     cd /www/server/panel/plugin
     if [ ! -d "/masterslave" ]; then
         wget -O vip_plugin.zip https://raw.githubusercontent.com/vanbac91/Crack_BT_Panel2020/master/update_pro/vip_plugin.zip
@@ -165,7 +165,7 @@ vip_plugin(){
     cd /root
 }
 
-#正式安装
+#Official installation
 if [[ ${OS} == 'CentOS' ]] && [[ ${CentOS_Version} -eq "7" ]]; then
     yum install epel-release wget curl nss fail2ban unzip lrzsz vim* -y
     yum update -y
@@ -198,5 +198,5 @@ fi
 
 clean_up
 
-echo -e "${green}[完成] ${plain}宝塔面板破解版已安装成功！"
-echo "按脚本提供的后台入口、账号、密码，登录宝塔面板并使用！"
+echo -e "${green}[carry out] ${plain}The cracked version of the pagoda panel has been installed successfully！"
+echo "Log in to the panel and use it according to the background entrance, account number, and password provided by the script!"
